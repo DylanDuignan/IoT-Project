f9c504d9a3b6926145833c897bec5b0304c8aca7
ConorCollier1 <98527408+ConorCollier1@users.noreply.github.com>
2022-04-28 11:34:00 +0100

    Update Home_Safe_Home.ino

--- Home_Safe_Home.ino
+++ Home_Safe_Home.ino
@@ -1,18 +1,16 @@
-
 #include <Keypad.h>
 #include <Wire.h>
 #include "rgb_lcd.h"
 
 rgb_lcd lcd;
 
-const int colorR = 0;
-const int colorG = 255;
+const int colorR = 255;
+const int colorG = 0;
 const int colorB = 0;
 const byte ROWS = 4;                /*Define the number of rows*/
 const byte COLS = 4;                /*Define the number of columns*/
-const char PASSWORD[4] = "2085";    /*Define the password*/
+const char PASSWORD[4] = "1245";    /*Define the password*/
 double cashMoney = 0;                     /*Define Balance*/
-char userInput = 0;
 
 char keys[ROWS][COLS] = {
   {'1','2','3', 'A'},
@@ -92,25 +90,41 @@ void loop() {
                 lcd.print("1. Depo/Withdraw");
                 lcd.setCursor(0,1);
                 lcd.print("2. Check Balance");
-              char userChoice = keypad.getKey();
-              Serial.println(userChoice);
-              if(userChoice){
-          Serial.begin(9600);
-          // set up the LCD's number of columns and rows:
-        
-          if (userChoice == '1')
-          {
-             lcd.setCursor(0,0);
-              lcd.clear();
-                lcd.print("1. Deposit");
-                lcd.setCursor(0,1);
-                lcd.print("2. Withdraw");
+
+
+char input1 = keypad.getKey();     /*Get received character from keypad*/
+
+    
+          Serial.println(input1);
+          if(input1){
+            
           }
-            }
+                if(input1 = "1")
+                {
+                 do
+                 {
+                 lcd.clear();
+                 lcd.setCursor(0,0);
+                 lcd.print("1=5 2=10 4=20 5=50");
+                 lcd.setCursor(0,1);
+                 lcd.print("7=100 8=500 0=Exit");
+                 char userInput = keypad.getKey() ;
+                 Serial.println(userInput);
+                 switch(userInput)
+                 case "1": cashMoney =cashMoney+ 5; break;
+                 case "2": cashMoney =cashMoney+ 10; break; 
+                 case "4": cashMoney =cashMoney+ 20; break;
+                 case "5": cashMoney =cashMoney+ 50; break;
+                 case "7": cashMoney =cashMoney+ 100; break;
+                 case "8": cashMoney =cashMoney+ 200; break;
+                 case "0": break; 
+                 default: lcd.clear(); lcd.setCursor(0.0); lcd.print("Error");
+                }
+                 while(userInput != 0 )
+          }}
           else{
-            lcd.setRGB(255, 0, 0);
             lcd.setCursor(0,1);
-                lcd.print("Invalid PIN");
+                lcd.print("NO!!!!!!!!!!!!!!!!!!!!");
                 
                 for(int i=0; i<4; i++)
                 {
@@ -121,8 +135,9 @@ void loop() {
          }
           } 
           }
-          }
-          }    
+          
+    }
+
 
 
 
